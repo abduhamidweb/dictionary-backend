ae3f3a9db46b42689e7bbe516148c126317b0518
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-06-25 22:32:33 +0500

    variants and some functions added

--- src/controllers/start.contr.ts
+++ src/controllers/start.contr.ts
@@ -2,16 +2,19 @@ import { Request, Response } from 'express';
 import Book from '../schemas/book.schema.js';
 import Unit from '../schemas/unit.schema.js';
 import Word from '../schemas/word.schema.js';
+import { shuffleArray } from '../utils/shuffle.js';
 
 class StartController {
     public async start(req: Request, res: Response) {
-        const { booksId, unitsId, wordCount } = req.body;
+        const { booksId, unitsId, wordCount, sort } = req.body;
         try {
             // req.body bo'lishi shart.
             if (!booksId) {
                 throw new Error("booksId is required");
             }
-
+            if (!sort) {
+                throw new Error("sort is required");
+            }
             if (!unitsId) {
                 throw new Error("unitsId is required");
             }
@@ -24,8 +27,8 @@ class StartController {
             }
             // Kitoblarni, unitlarni va so'zlarni topish uchun funksiyalarni chaqiring
             const foundBooks = await findBooksByIds(booksId);
-            const matchingUnits =await findUnitsByBookIdsAndUnitIds(foundBooks, unitsId);
-            const matchingWords =await findWordsByUnitIdsAndWordCount( matchingUnits, wordCount);
+            const matchingUnits = await findUnitsByBookIdsAndUnitIds(foundBooks, unitsId);
+            const matchingWords = await findWordsByUnitIdsAndWordCount(matchingUnits, wordCount, sort);
 
             // Teng kelgan so'zlarni JSON formatida javob qiling
             res.json(matchingWords);
@@ -36,12 +39,11 @@ class StartController {
     }
 }
 export default new StartController();
-// Kitoblarni IDlariga ko'ra qidiruv
+// Kitoblarni IDlariga ko'ra qidiruv ---
 async function findBooksByIds(bookIds: any) {
     const books = await Book.find();
     return books.filter((book: any) => bookIds.includes(book._id.toString()));
 }
-
 async function findUnitsByBookIdsAndUnitIds(books: any[], unitIds: any[]) {
     const matchingUnits: any[] = [];
     for (const book of books) {
@@ -57,15 +59,74 @@ async function findUnitsByBookIdsAndUnitIds(books: any[], unitIds: any[]) {
     }
     return matchingUnits;
 }
-
-async function findWordsByUnitIdsAndWordCount(units: any[], wordCount: number) {
+async function findWordsByUnitIdsAndWordCount(units: any[], wordCount: number, sort:string) {
     const words = await Word.find();
     const matchingWords: any[] = [];
     for (const unit of units) {
         const unitWords = words.filter((word: any) => unit.words.includes(word._id));
-        const slicedWords = unitWords.slice(0, wordCount);
-        matchingWords.push(...slicedWords);
-    }
-    return matchingWords;
-}
 
+        matchingWords.push(...unitWords);
+    }
+    const rearrangedArray = matchingWords.map(item => {
+        return {
+            "_id": item._id,
+            "engWord": item.engWord,
+            "uzbWord": item.uzbWord,
+            "unitId": item.unitId
+        };
+    });
+    function mergeWords(data: any) {
+        let engWords = [];
+        let uzbWords = [];
+
+        for (let i = 0; i < data.length; i++) {
+            engWords.push(data[i].engWord);
+            uzbWords.push(data[i].uzbWord);
+        }
+
+        return { engWords, uzbWords };
+    }
+
+    let shuffledArray = shuffleArray(rearrangedArray, wordCount)
+    const { engWords, uzbWords } = mergeWords(rearrangedArray);
+    // Uzbekcha test
+    let addedVariantsUzb = shuffledArray.map((obj) => {
+        return { ...obj, variants: shuffleArray(uzbWords, 3) };
+    });
+    let filterUzbWords = addedVariantsUzb.map((obj) => {
+        obj.variants.push(obj.uzbWord)
+        return obj
+    });
+    const deduplicatedArrayUzb = filterUzbWords.map(obj => {
+        return {
+            ...obj,
+            variants: Array.from(new Set(obj.variants)),
+            role: "uzb"
+
+        };
+    });
+
+    // inglizcha test
+    let addedVariantsEng = shuffledArray.map((obj) => {
+        return { ...obj, variants: shuffleArray(engWords, 3) };
+    });
+    let filterEngWords = addedVariantsEng.map((obj) => {
+        obj.variants.push(obj.engWord)
+        return obj
+    });
+    const deduplicatedArrayEng = filterEngWords.map(obj => {
+        return {
+            ...obj,
+            variants: Array.from(new Set(obj.variants)),
+            role:"eng"
+        };
+    });
+    // ikkisidaham bor bo'lgan arraylarni render qilib chiqarish kerak.
+    const randomWords = shuffleArray(deduplicatedArrayEng.concat(deduplicatedArrayUzb), wordCount)
+
+    // return qilayotganda deduplicatedArrayEng, deduplicatedArrayUzb, randomWords ni render qilsa o'ladi.
+    // user nechtadir so'zni so'rasa o'shancha so'z random bo'ladi.
+    if (sort == "uzb") return deduplicatedArrayUzb
+    else if (sort == "eng") return deduplicatedArrayEng
+    else if (sort =="random") return randomWords
+}
